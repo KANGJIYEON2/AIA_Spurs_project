cf4466fad413be5c6f6c499964996b880a11277d
leegyurye <lgr0813@naver.com>
2023-08-23 12:02:34 +0900

    Footer code update

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -4,43 +4,49 @@ import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import "../globals.css";
 
-const primary = {
-  backgroundColor: "#333D47",
-};
-
 export default function Footer() {
+  const FooterInfo = (
+    <Grid item xs={8} paddingBottom={1}>
+      <Typography sx={{ fontSize: 14 }} color="#fafafa">
+        AIA 생명보험 주식회사 <br />
+        서울시 중구 통일로2길 16(순화동 216) AIA타워 <br />
+        (우)04511 대표이사 네이슨 황
+      </Typography>
+    </Grid>
+  );
+
+  const Links = (menu: string, path: string) => {
+    return (
+      <Link color="#fafafa" href={path} underline="none">
+        {menu} <br />
+      </Link>
+    );
+  };
+
+  const FooterLink = (
+    <Grid item xs={4}>
+      <Typography sx={{ fontSize: 14 }} align="right">
+        {Links("서비스 이용 약관", "/terms")}
+        {Links(
+          "개인정보처리방침",
+          "https://www.aia.co.kr/ko/index/pi-processing.html"
+        )}
+        {Links("관리자페이지", "/login2")}
+      </Typography>
+    </Grid>
+  );
+
   return (
     <Grid
       className="Footer"
       container
       spacing={1}
       sx={{
-        bgcolor: primary,
+        bgcolor: "#333D47",
       }}
       padding={1}>
-      <Grid item xs={8} paddingBottom={1}>
-        <Typography sx={{ fontSize: 14 }} color="#fafafa">
-          AIA 생명보험 주식회사 <br />
-          서울시 중구 통일로2길 16(순화동 216) AIA타워 <br />
-          (우)04511 대표이사 네이슨 황
-        </Typography>
-      </Grid>
-      <Grid item xs={4}>
-        <Typography sx={{ fontSize: 14 }} color="#fafafa" align="right">
-          <Link color="#fafafa" href="/terms" underline="none">
-            서비스 이용 약관 <br />
-          </Link>
-          <Link
-            color="#fafafa"
-            href="https://www.aia.co.kr/ko/index/pi-processing.html"
-            underline="none">
-            개인정보처리방침 <br />
-          </Link>
-          <Link color="#fafafa" href="/login2" underline="none">
-            관리자페이지
-          </Link>
-        </Typography>
-      </Grid>
+      {FooterInfo}
+      {FooterLink}
     </Grid>
   );
 }
