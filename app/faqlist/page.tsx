2858f813ef449224c3b36f7f2254e2bc1b7727b8
KANGJIYEON2 <hihello516@naver.com>
2023-08-24 17:19:05 +0900

    update db

--- app/faqlist/page.tsx
+++ app/faqlist/page.tsx
@@ -1,41 +1,7 @@
 "use client";
+import Link from "next/link";
 import React, { FormEvent, useEffect, useState } from "react";
-import {
-  Paper,
-  Table,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  TablePagination,
-  IconButton,
-} from "@mui/material";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-
-const Faqlists = () => {
-  const columns = [
-    { id: "id", name: "ID" },
-    { id: "title", name: "title" },
-    { id: "explain", name: "explain" },
-    { id: "ETC", name: "관련설명" },
-  ];
-
-  const handlechangepage = (event: any, newpage: any) => {
-    pagechange(newpage);
-  };
-  const handleRowsPerPage = (event: any) => {
-    rowperpagechange(+event.target.value);
-    pagechange(0);
-  };
-
-  const [rows, rowchange] = useState([]);
-  const [page, pagechange] = useState(0);
-  const [rowperpage, rowperpagechange] = useState(5);
-  const [openModule, setOpenModule] = React.useState(false);
-
-  useEffect(() => {
+/*useEffect(() => {
     fetch("http://localhost:8000/faqlist")
       .then((resp) => {
         return resp.json();
@@ -47,52 +13,27 @@ const Faqlists = () => {
         console.log(e.message);
       });
   }, []);
+*/
+const Faqlists = () => {
+ 
+  return <div>
+    <main>
+    <div className="faqtem">
+  <h3>로그인하는 방법</h3>
+    <p>로그인 하는 방법에 대하여 설명드리겠습니다.</p>
+    </div>
+    <div className="faqtem">
+  <h3>회원하는 방법</h3>
+    <p>회원가입 하는 방법에 대하여 설명드리겠습니다.</p>
+    </div>
+    <div className="faqtem">
+  <h3>이벤트참여하는 방법</h3>
+    <p>이벤트 참여하는 방법에 대하여 설명드리겠습니다.</p>
+    </div>
+    </main>
 
-  return (
-    <Container sx={{ minWidth: "sm", maxWidth: "xl" }}>
-      <Box sx={{ marginTop: 5, marginBottom: 30 }}>
-        <h3>FAQ리스트</h3>
-        <Paper sx={{ width: "90%" }}>
-          <TableContainer sx={{ maxHeight: 450 }}>
-            <Table stickyHeader>
-              <TableHead>
-                <TableRow>
-                  {columns.map((column) => (
-                    <TableCell key={column.id}>{column.name}</TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows &&
-                  rows
-                    .slice(page * rowperpage, page * rowperpage + rowperpage)
-                    .map((row, i) => {
-                      return (
-                        <TableRow key={i}>
-                          {columns &&
-                            columns.map((column, i) => {
-                              let value = row[column.id];
-                              return (
-                                <TableCell key={value}>{value} </TableCell>
-                              );
-                            })}
-                        </TableRow>
-                      );
-                    })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 25]}
-            page={page}
-            count={rows.length}
-            rowsPerPage={rowperpage}
-            component="div"
-            onPageChange={handlechangepage}
-            onRowsPerPageChange={handleRowsPerPage}></TablePagination>
-        </Paper>
-      </Box>
-    </Container>
-  );
+  </div>
+   
+  
 };
 export default Faqlists;
