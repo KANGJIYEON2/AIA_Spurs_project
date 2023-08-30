aead25c254e696c30bbb39445ee8ef228f03b25d
KANGJIYEON2 <hihello516@naver.com>
2023-08-30 10:12:36 +0900

    notice page

--- app/faqlist/page.tsx
+++ app/faqlist/page.tsx
@@ -1,97 +1,89 @@
 "use client";
-import React, { FormEvent, useEffect, useState } from "react";
-import {
-  Paper,
-  Table,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  TablePagination,
-} from "@mui/material";
-import Container from "@mui/material/Container";
+
+import * as React from "react";
 import Box from "@mui/material/Box";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import Typography from "@mui/material/Typography";
 
-const Notice = () => {
-  const columns = [
-    { id: "id", name: "ID" },
-    { id: "title", name: "title" },
-    { id: "explain", name: "explain" },
-    { id: "ETC", name: "관련설명" },
-  ];
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "title",
+    headerName: "title",
+    width: 200,
+    editable: true,
+  },
+  {
+    field: "contents",
+    headerName: "contents",
+    width: 550,
+    editable: true,
+  },
+  {
+    field: "note",
+    headerName: "note",
+    type: "string",
+    width: 200,
+    editable: true,
+  },
+];
 
-  const handlechangepage = (event: any, newpage: any) => {
-    pagechange(newpage);
-  };
-  const handleRowsPerPage = (event: any) => {
-    rowperpagechange(+event.target.value);
-    pagechange(0);
-  };
+const rows = [
+  {
+    id: 1,
+    title: "로그인 하는 방법",
+    contents: "로그인을 하기 위해서는 LOGIN 창에 접속해야 합니다.",
+    note: "로그인 관련 문의",
+  },
+  {
+    id: 2,
+    title: "이벤트 참여하는 방법",
+    contents: "이벤트에 참여하기 위해서는 EVENT 창에 접속해야 합니다.",
+    note: "이벤트 관련 문의",
+  },
+  {
+    id: 3,
+    title: "비밀번호 찾는 방법",
+    contents:
+      "비밀번호를 찾기 위해서는 해당 SNS에서 비밀번호 찾기를 진행해야 합니다.",
+    note: "로그인 관련 문의",
+  },
+  {
+    id: 4,
+    title: "영상 재생이 안되는 경우",
+    contents:
+      "원활한 영상 재생을 위해서는 Chrome 브라우저 접속을 권장하고 있습니다.",
+    note: "콘텐츠 관련 문의",
+  },
+  {
+    id: 5,
+    title: "사진 다운로드가 안되는 경우",
+    contents:
+      "원활한 콘텐츠 재생을 위해서는 Chrome 브라우저 접속을 권장하고 있습니다.",
+    note: "콘텐츠 관련 문의",
+  },
+];
 
-  const [rows, rowchange] = useState([]);
-  const [page, pagechange] = useState(0);
-  const [rowperpage, rowperpagechange] = useState(5);
-  const [openModule, setOpenModule] = React.useState(false);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/faqlist")
-      .then((resp) => {
-        return resp.json();
-      })
-      .then((resp) => {
-        rowchange(resp);
-      })
-      .catch((e) => {
-        console.log(e.message);
-      });
-  }, []);
-
+export default function DataGridDemo() {
   return (
-    <Container sx={{ minWidth: "sm", maxWidth: "xl" }}>
-      <Box sx={{ marginTop: 5, marginBottom: 30 }}>
-        <h3>AIA-SPURS 공지사항</h3>
-        <Paper sx={{ width: "90%" }}>
-          <TableContainer sx={{ maxHeight: 450 }}>
-            <Table stickyHeader>
-              <TableHead>
-                <TableRow>
-                  {columns.map((column) => (
-                    <TableCell key={column.id}>{column.name}</TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows &&
-                  rows
-                    .slice(page * rowperpage, page * rowperpage + rowperpage)
-                    .map((row, i) => {
-                      return (
-                        <TableRow key={i}>
-                          {columns &&
-                            columns.map((column, i) => {
-                              let value = row[column.id];
-                              return (
-                                <TableCell key={value}>{value} </TableCell>
-                              );
-                            })}
-                        </TableRow>
-                      );
-                    })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 25]}
-            page={page}
-            count={rows.length}
-            rowsPerPage={rowperpage}
-            component="div"
-            onPageChange={handlechangepage}
-            onRowsPerPageChange={handleRowsPerPage}></TablePagination>
-        </Paper>
-      </Box>
-    </Container>
+    <Box sx={{ width: "100%" }}>
+      <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+        AIA-SPURS 관련 FAQ
+      </Typography>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
   );
-};
-export default Notice;
+}
--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -1,97 +1,47 @@
 "use client";
 import React, { FormEvent, useEffect, useState } from "react";
-import {
-  Paper,
-  Table,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  TablePagination,
-} from "@mui/material";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
-const Notice = () => {
+
+export default function Notice() {
   const columns = [
-    { id: "id", name: "ID" },
-    { id: "title", name: "title" },
-    { id: "explain", name: "explain" },
-    { id: "ETC", name: "관련설명" },
+    { field: "id", name: "id", width: 90  } as any,
+    { field: "title", name: "title" , width: 200}  as any,
+    { field: "explain", name: "explain", width: 550 }  as any,
+    { field: "ETC", name: "관련설명", width: 200 }  as any,
   ];
-
-  const handlechangepage = (event: any, newpage: any) => {
-    pagechange(newpage);
-  };
-  const handleRowsPerPage = (event: any) => {
-    rowperpagechange(+event.target.value);
-    pagechange(0);
-  };
-
+  
   const [rows, rowchange] = useState([]);
-  const [page, pagechange] = useState(0);
-  const [rowperpage, rowperpagechange] = useState(5);
-  const [openModule, setOpenModule] = React.useState(false);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/noticelist")
-      .then((resp) => {
-        return resp.json();
-      })
-      .then((resp) => {
-        rowchange(resp);
-      })
-      .catch((e) => {
-        console.log(e.message);
-      });
-  }, []);
-
+  
+useEffect(() => {
+  fetch("http://localhost:8000/noticelist")
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((resp) => {
+      rowchange(resp);
+    })
+    .catch((e) => {
+      console.log(e.message);
+    });
+}, []);
   return (
-    <Container sx={{ minWidth: "sm", maxWidth: "xl" }}>
-      <Box sx={{ marginTop: 5, marginBottom: 30 }}>
-        <h3>AIA-SPURS 공지사항</h3>
-        <Paper sx={{ width: "90%" }}>
-          <TableContainer sx={{ maxHeight: 450 }}>
-            <Table stickyHeader>
-              <TableHead>
-                <TableRow>
-                  {columns.map((column) => (
-                    <TableCell key={column.id}>{column.name}</TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows &&
-                  rows
-                    .slice(page * rowperpage, page * rowperpage + rowperpage)
-                    .map((row, i) => {
-                      return (
-                        <TableRow key={i}>
-                          {columns &&
-                            columns.map((column, i) => {
-                              let value = row[column.id];
-                              return (
-                                <TableCell key={value}>{value} </TableCell>
-                              );
-                            })}
-                        </TableRow>
-                      );
-                    })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 25]}
-            page={page}
-            count={rows.length}
-            rowsPerPage={rowperpage}
-            component="div"
-            onPageChange={handlechangepage}
-            onRowsPerPageChange={handleRowsPerPage}></TablePagination>
-        </Paper>
-      </Box>
-    </Container>
+    <Box sx={{ minWidth: "sm", maxWidth: "xl" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
   );
-};
-export default Notice;
+}
