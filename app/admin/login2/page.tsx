016fddbacb8c1a30e684c473c40328733625e4e7
leegyurye <lgr0813@naver.com>
2023-09-22 10:32:30 +0900

    login error update

--- app/admin/login2/page.tsx
+++ app/admin/login2/page.tsx
@@ -13,108 +13,95 @@ import { NextPage } from "next";
 import { signIn } from "next-auth/react";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#001B3F",
-            contrastText: "#fafafa",
-        },
+  palette: {
+    primary: {
+      main: "#001B3F",
+      contrastText: "#fafafa",
     },
+  },
 });
 
 const Login2: NextPage = (props): JSX.Element => {
-    const [id, setId] = useState("");
-    const [password, setPassword] = useState("");
-    const [blankPw, setBlankPw] = useState(false);
-    const [blankId, setBlankId] = useState(false);
-    const [error, setError] = useState(false);
-    const router = useRouter();
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [blankPw, setBlankPw] = useState(false);
+  const [blankId, setBlankId] = useState(false);
+  const [error, setError] = useState(false);
+  const router = useRouter();
 
-    const onChangeId = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setId(event.target.value);
-    };
+  const onChangeId = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setId(event.target.value);
+  };
 
-    const onChangePw = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(event.target.value);
-    };
+  const onChangePw = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
 
-    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if (id.length <= 0) setBlankId(true);
-        if (password.length <= 0) setBlankPw(true);
-        if (!blankId && !blankPw) {
-            await signIn("credentials", {
-                id: id,
-                password: password,
-                redirect: false,
-            }).then((result) => {
-                if (result?.ok) router.push("/admin");
-                if (result?.error) setError(true);
-            });
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (id.length <= 0) setBlankId(true);
+    if (password.length <= 0) setBlankPw(true);
+    if (!blankId && !blankPw) {
+      await signIn("credentials", {
+        id: id,
+        password: password,
+        redirect: false,
+      }).then((result) => {
+        if (result?.ok) router.push("/admin");
+        if (result?.error) {
+          setError(true);
+          router.push("/admin/login2");
         }
-    };
+      });
+    }
+  };
 
-    return (
-        <Container sx={{ minWidth: "sm", maxWidth: "xl" }}>
-            <Grid
-                container
-                display="flex"
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-                sx={{ m: 5, height: "75vh" }}
-            >
-                <Box
-                    component="form"
-                    autoComplete="off"
-                    onSubmit={handleSubmit}
-                >
-                    <Box>
-                        <TextField
-                            label="id"
-                            variant="outlined"
-                            value={id}
-                            onChange={onChangeId}
-                            onBlur={() => setBlankId(id.length <= 0)}
-                        />
-                        {blankId && (
-                            <Alert severity="error">
-                                아이디를 입력해주세요
-                            </Alert>
-                        )}
-                    </Box>
-                    <Box sx={{ marginTop: 1, marginBottom: 1 }}>
-                        <TextField
-                            type="password"
-                            label="password"
-                            variant="outlined"
-                            value={password}
-                            onChange={onChangePw}
-                            onBlur={() => setBlankPw(password.length <= 0)}
-                        />
-                        {blankPw && (
-                            <Alert severity="error">
-                                비밀번호를 입력해주세요
-                            </Alert>
-                        )}
-                    </Box>
-                    <ThemeProvider theme={theme}>
-                        <Box>
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                sx={{ width: 225, height: 55 }}
-                            >
-                                LOGIN
-                            </Button>
-                        </Box>
-                    </ThemeProvider>
-                    {error && (
-                        <Alert severity="error">비밀번호를 확인해주세요</Alert>
-                    )}
-                </Box>
-            </Grid>
-        </Container>
-    );
+  return (
+    <Container sx={{ minWidth: "sm", maxWidth: "xl" }}>
+      <Grid
+        container
+        display="flex"
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ m: 5, height: "75vh" }}>
+        <Box component="form" autoComplete="off" onSubmit={handleSubmit}>
+          <Box>
+            <TextField
+              label="id"
+              variant="outlined"
+              value={id}
+              onChange={onChangeId}
+              onBlur={() => setBlankId(id.length <= 0)}
+            />
+            {blankId && <Alert severity="error">아이디를 입력해주세요</Alert>}
+          </Box>
+          <Box sx={{ marginTop: 1, marginBottom: 1 }}>
+            <TextField
+              type="password"
+              label="password"
+              variant="outlined"
+              value={password}
+              onChange={onChangePw}
+              onBlur={() => setBlankPw(password.length <= 0)}
+            />
+            {blankPw && <Alert severity="error">비밀번호를 입력해주세요</Alert>}
+          </Box>
+          <ThemeProvider theme={theme}>
+            <Box>
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{ width: 225, height: 55 }}>
+                LOGIN
+              </Button>
+            </Box>
+          </ThemeProvider>
+          {error && <Alert severity="error">비밀번호를 확인해주세요</Alert>}
+        </Box>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Login2;
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,79 +5,79 @@ import NaverProvider from "next-auth/providers/naver";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                id: { label: "Username", type: "text" },
-                password: { label: "Password", type: "password" },
-            },
-            async authorize(credentials) {
-                const userId = credentials?.id;
-                const userPassword = credentials?.password;
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        id: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const userId = credentials?.id;
+        const userPassword = credentials?.password;
 
-                if (!userId || !userPassword) {
-                    return null;
-                }
+        if (!userId || !userPassword) {
+          return null;
+        }
 
-                const res = await fetch(
-                    `http://localhost:8000/User?id=${userId}&password=${userPassword}`
-                );
+        const res = await fetch(
+          `http://localhost:8000/User?id=${userId}&password=${userPassword}`
+        );
 
-                if (res.status !== 200) {
-                    return null;
-                }
+        if (res.status !== 200) {
+          return null;
+        }
 
-                const users = await res.json();
-                const user = users[0];
+        const users = await res.json();
+        const user = users[0];
 
-                console.log("Server response status:", res.status);
-                console.log("User data:", user);
+        console.log("Server response status:", res.status);
+        console.log("User data:", user);
 
-                if (
-                    user &&
-                    user.id === credentials.id &&
-                    user.password === credentials.password
-                ) {
-                    return { id: user.id, name: user.name, email: user.email };
-                } else {
-                    return null;
-                }
-            },
-        }),
+        if (
+          user &&
+          user.id === credentials.id &&
+          user.password === credentials.password
+        ) {
+          return { id: user.id, name: user.name, email: user.email };
+        } else {
+          return null;
+        }
+      },
+    }),
 
-        KakaoProvider({
-            clientId: process.env.KAKAO_CLIENT_ID as string,
-            clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
-        }),
-        NaverProvider({
-            clientId: process.env.NAVER_CLIENT_ID as string,
-            clientSecret: process.env.NAVER_CLIENT_SECRET as string,
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-    ],
-    session: {
-        strategy: "jwt",
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID as string,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
+    }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID as string,
+      clientSecret: process.env.NAVER_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/api/auth/login" || "/admin/login2",
+  },
+  callbacks: {
+    async jwt({ token, user, trigger, session }) {
+      if (trigger === "update") {
+        return { ...token, ...session.user };
+      }
+      return { ...token, ...user };
     },
-    pages: {
-        signIn: "/api/auth/login",
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
     },
-    callbacks: {
-        async jwt({ token, user, trigger, session }) {
-            if (trigger === "update") {
-                return { ...token, ...session.user };
-            }
-            return { ...token, ...user };
-        },
-        async session({ session, token }) {
-            session.user = token as any;
-            return session;
-        },
-    },
-    secret: process.env.NEXT_AUTH_SECRET,
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
