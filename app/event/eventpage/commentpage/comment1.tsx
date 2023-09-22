b5e7f6b0031bbf4153495dbf036e08f46be5c23a
KANGJIYEON2 <hihello516@naver.com>
2023-09-22 14:30:38 +0900

    update layout & comment

--- app/event/eventpage/commentpage/comment1.tsx
+++ app/event/eventpage/commentpage/comment1.tsx
@@ -1,28 +1,32 @@
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-
-const Comment = ({comment, replies}:any) => {
-  return (
-    <div className="comment">
-      <div className="comment-image-container">
-    <AccountCircleIcon />
-      </div>
-      <div className='comment-right-part'>
-        <div className='comment-content'>
-        <div className='comment-author'>{comment.username}</div>
-      <div>{comment.createdAt}</div>
-      </div>
-      <div className='comment-text'> {comment.body} </div>
-      {replies.length > 0 && (
-        <div className='replies'>
-          {replies.map((reply:any) => (
-            <Comment comment={reply} key={reply.id} replies={[]} />
-          ))}
-  </div>
-      )}
-    </div>
-    </div>
-  )
-}
+const Comment = ({ comment, replies }: any) => {
+    return (
+        <div className="comment">
+            <div className="comment-right-part">
+                <div className="comment-content">
+                    <div className="comment-author">
+                        {""}
+                        <AccountCircleIcon />
+                        {comment.username}
+                    </div>
+                    <div>{comment.createdAt}</div>
+                </div>
+                <div className="comment-text"> {comment.body} </div>
+                {replies.length > 0 && (
+                    <div className="replies">
+                        {replies.map((reply: any) => (
+                            <Comment
+                                comment={reply}
+                                key={reply.id}
+                                replies={[]}
+                            />
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default Comment;
--- app/event/eventpage/commentpage/commentForm1.tsx
+++ app/event/eventpage/commentpage/commentForm1.tsx
@@ -1,26 +1,49 @@
-import { Height } from "@mui/icons-material";
 import { useState } from "react";
+import { Box, TextField, Button, Grid } from "@mui/material";
 
-
-const CommentForm = ({handleSubmit,submitLabel}:any) => {
-  const [text, setText] = useState("");
-  const isTextareaDisabled = text.length === 0 ;
-  const onsubmit = (event:any) => {
-    event.preventDefault();
-    handleSubmit(text);
-    setText("");
-  }
-  return (
-    <form onSubmit={onsubmit} >
-        <textarea
-          style={{resize:"none" , height:100, width:"50%"}}
-          className="comment-form-textarea"
-          value={text}
-          onChange={(e) => setText(e.target.value)} />
-          <button className="comment-form-button" disabled={isTextareaDisabled} 
-          style={{ margin: 3, padding:2,}}>{submitLabel}</button>
-    </form>
-  )
-}
+const CommentForm = ({ handleSubmit, submitLabel }: any) => {
+    const [text, setText] = useState("");
+    const isTextareaDisabled = text.length === 0;
+    const onsubmit = (event: any) => {
+        event.preventDefault();
+        handleSubmit(text);
+        setText("");
+    };
+    return (
+        <Box component="form" onSubmit={onsubmit} mb={5}>
+            <Grid container>
+                <Grid item xs={5}>
+                    <Box
+                        sx={{
+                            width: 700,
+                            maxWidth: "100%",
+                        }}
+                    >
+                        <TextField
+                            fullWidth
+                            focused
+                            color="info"
+                            id="comment-form-textarea"
+                            value={text}
+                            onChange={(e) => setText(e.target.value)}
+                        />
+                    </Box>
+                </Grid>
+                <Grid item xs={7}>
+                    <button
+                        className="comment-form-button"
+                        disabled={isTextareaDisabled}
+                        style={{
+                            padding: 18,
+                        }}
+                        color="primary"
+                    >
+                        {submitLabel}
+                    </button>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
 
 export default CommentForm;
