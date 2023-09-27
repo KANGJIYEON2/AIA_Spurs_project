632dd362fa664df752a60fd5a290d46cfe3ee70c
KANGJIYEON2 <hihello516@naver.com>
2023-09-27 11:36:36 +0900

    update

--- app/allaboutspurs/page.tsx
+++ app/allaboutspurs/page.tsx
@@ -13,46 +13,67 @@ import "../globals.css";
 import Container from "@mui/material/Container";
 
 export default function LabTabs() {
-  const [value, setValue] = React.useState("1");
+    const [value, setValue] = React.useState("1");
 
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+    };
+    const [mounted, setMounted] = React.useState(false);
 
-  return (
-    <Container sx={{ minWidth: "sm", maxWidth: "xl" }}>
-      <Box className="Tab" sx={{ width: "100%", typography: "body1" }}>
-        <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <TabList onChange={handleChange} aria-label="lab API tabs example">
-              <Tab
-                label="SPURS 선수들의 특별 영상"
-                value="1"
-                style={{ fontSize: "0.9rem", fontWeight: "bold" }}
-              />
-              <Tab
-                label="SPURS 선수들의 건강팁"
-                value="2"
-                style={{ fontSize: "0.9rem", fontWeight: "bold" }}
-              />
-              <Tab
-                label="버추얼 스타디움 투어"
-                value="3"
-                style={{ fontSize: "0.9rem", fontWeight: "bold" }}
-              />
-            </TabList>
-          </Box>
-          <TabPanel value="1">
-            <Special />
-          </TabPanel>
-          <TabPanel value="2">
-            <Healthtip />
-          </TabPanel>
-          <TabPanel value="3">
-            <Tour />
-          </TabPanel>
-        </TabContext>
-      </Box>
-    </Container>
-  );
+    React.useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return null;
+    }
+
+    return (
+        <Container sx={{ minWidth: "sm", maxWidth: "xl" }}>
+            <Box className="Tab" sx={{ width: "100%", typography: "body1" }}>
+                <TabContext value={value}>
+                    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                        <TabList
+                            onChange={handleChange}
+                            aria-label="lab API tabs example"
+                        >
+                            <Tab
+                                label="SPURS 선수들의 특별 영상"
+                                value="1"
+                                style={{
+                                    fontSize: "0.9rem",
+                                    fontWeight: "bold",
+                                }}
+                            />
+                            <Tab
+                                label="SPURS 선수들의 건강팁"
+                                value="2"
+                                style={{
+                                    fontSize: "0.9rem",
+                                    fontWeight: "bold",
+                                }}
+                            />
+                            <Tab
+                                label="버추얼 스타디움 투어"
+                                value="3"
+                                style={{
+                                    fontSize: "0.9rem",
+                                    fontWeight: "bold",
+                                }}
+                            />
+                        </TabList>
+                    </Box>
+                    <TabPanel value="1">
+                        <Special />
+                    </TabPanel>
+                    <TabPanel value="2">
+                        <Healthtip />
+                    </TabPanel>
+                    <TabPanel value="3">
+                        <Tour />
+                    </TabPanel>
+                </TabContext>
+            </Box>
+        </Container>
+    );
 }
