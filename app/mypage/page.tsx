e3c9cce722b53912abda8c21dc65b2df4447980e
leegyurye <lgr0813@naver.com>
2023-09-27 13:58:24 +0900

    mypage layout update

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,20 +1,76 @@
 "use client";
 
-import React from "react";
-import Mypage from "./Mypage";
-import Login from "../api/auth/login/page";
-import { useSession } from "next-auth/react";
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
 
-const My_page = () => {
+const style = {
+  width: "100%",
+  maxWidth: 360,
+  bgcolor: "background.paper",
+};
+
+const Mypage = () => {
   const { data: session } = useSession();
+  console.log("Session Data:", session);
+  const userProfileImg = session?.user?.image as string;
 
   return (
-    <main>
-      <Mypage />
-      {/* {session && <Mypage />}
-      {!session && <Login />} */}
-    </main>
+    <Container sx={{ minWidth: "sm", maxWidth: "lg", marginLeft: 15 }}>
+      <Typography
+        variant="h4"
+        sx={{ marginLeft: 5, marginTop: 5, fontWeight: "Bold" }}>
+        프로필
+      </Typography>
+      <Box display="flex" sx={{ marginLeft: 5, marginTop: 5, height: "30vh" }}>
+        <Box justifyContent="flex-start" sx={{ marginLeft: 2, flexGrow: 1 }}>
+          <Avatar
+            alt={session?.user?.name as string}
+            src={userProfileImg}
+            sx={{ width: 200, height: 200 }}
+          />
+        </Box>
+        <Box alignItems="center" sx={{ flexGrow: 15, paddingTop: 7 }}>
+          <Typography variant="h6">Name : {session?.user?.name}</Typography>
+          <br />
+          <Typography variant="h6">Email : {session?.user?.email}</Typography>
+        </Box>
+      </Box>
+      <Typography variant="h4" sx={{ marginLeft: 5, fontWeight: "Bold" }}>
+        개인정보수집처리동의
+      </Typography>
+      <Box sx={{ marginLeft: 5, marginTop: 1 }}>
+        <Grid container direction="row" alignItems="center">
+          <List sx={style} component="nav" aria-label="mailbox folders">
+            <Divider />
+            <ListItemButton href="https://aiaspurs.com/kr/pic">
+              <ListItemText primary="개인정보 수집∙이용동의 및 처리위탁(선택)" />
+            </ListItemButton>
+            <Divider />
+            <ListItemButton href="https://aiaspurs.com/kr/marketing">
+              <ListItemText primary="마케팅을 위한 광고성 정보 수신동의(선택)" />
+            </ListItemButton>
+          </List>
+        </Grid>
+      </Box>
+      <Box sx={{ m: 5 }}>
+        <Button onClick={() => (session ? signOut() : signIn())}>
+          <Typography color="#0f0f0f" variant="h5" sx={{ marginBottom: 10 }}>
+            탈퇴하기
+          </Typography>
+        </Button>
+      </Box>
+    </Container>
   );
 };
 
-export default My_page;
+export default Mypage;
